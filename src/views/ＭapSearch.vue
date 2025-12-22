53ada30e6e550f041ed18b4f3940804837d538a7
gtut_Betty <gtut3252300@gmail.com>
2025-12-22 22:37:05 +0800

    feat:新增結構

--- src/views/ＭapSearch.vue
+++ src/views/ＭapSearch.vue
@@ -1,13 +1,7 @@
 <template>
-<<<<<<< HEAD
   <div class="map-container">
     <el-container class="main-layout">
       <div class="sidebar" :class="{ active: sidebar.sidebarOpen }">
-=======
-<div class="map-container">
-    <el-container class="main-layout">
-       <div class="sidebar" :class="{ active: sidebar.sidebarOpen }">
->>>>>>> 1fdc2cd (feat:變更sass全域位置)
         <div
           class="title-container"
           v-for="item in title.Networks"
@@ -16,11 +10,7 @@
           <h2 class="title-container__lable">{{ item.NetworkName.Zh_tw }}</h2>
           <div class="title-container_sublable">{{ item.NetworkName.En }}</div>
         </div>
-<<<<<<< HEAD
         <!-- <div class="control-container">
-=======
-        <div class="control-container">
->>>>>>> 1fdc2cd (feat:變更sass全域位置)
           <el-radio-group
             v-model="vaildControl"
             fill="#3e835e"
@@ -30,7 +20,6 @@
             <el-radio-button label="去程" value="go" />
             <el-radio-button label="往返" value="back" />
           </el-radio-group>
-<<<<<<< HEAD
         </div> -->
 
         <el-scrollbar v-loading="loading">
@@ -62,27 +51,6 @@
       </div>
     </el-container>
   </div>
-=======
-        </div>
-
-        <el-scrollbar v-loading="loading">
-        
-        </el-scrollbar>
-      </div>
-        <div>
-   
-        <ul>
-        <!-- <li v-for="item in dataList.Routes" :key="item.RouteID">
-      {{ item.RouteName.Zh_tw }}
-        </li> -->
-        </ul>
-    </div>
-    </el-container>
-      </div>
-
-
-
->>>>>>> 1fdc2cd (feat:變更sass全域位置)
 </template>
 <script lang="ts" setup>
 import { ref, onMounted } from "vue";
@@ -91,7 +59,6 @@ import { useUiStore } from "@/stores/ui";
 import { useDataStore } from "@/stores/data";
 const { networkApi } = useDataStore();
 import { fetchTainanBusNetwork } from "@/utils/bus";
-<<<<<<< HEAD
 import tdxRequest from "@/api/tdxApi";
 const { routesCity } = fetchTainanBusNetwork();
 const title = ref([]);
@@ -109,18 +76,10 @@ const onChange = (status: boolean, index: number, name:string) => {
   }
 };
 
-=======
-const { routesCity } = fetchTainanBusNetwork();
-const title = ref([]);
-const dataList =ref([]);
-const loading = ref(true);
-const sidebar = useUiStore();
->>>>>>> 1fdc2cd (feat:變更sass全域位置)
 onMounted(() => {
   init();
 });
 
-<<<<<<< HEAD
 const filterIcon = ref(false);
 const handleFilter =()=>{
 filterIcon.value =  !filterIcon.value;
@@ -173,32 +132,3 @@ function routeApi() {
 }
 
 </style>
-=======
-function init() {
-  try {
-  routeApi();
-    networkApi().then((res) => {
-      title.value = res;
-    });
-  } catch (e) {
-    console.error(e);
-  }finally {
-    // loading.value = false;
-  }
-}
-
-function routeApi() {
-      // dataList.value = routesCity
-      
-//   tdxRequest
-//     .get("/Bus/Route/City/Tainan?%24top=30&%24format=JSON")
-//     .then((res) => {
-//         dataList.value = res.data;
-//       console.log(res.data.Routes);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-}
-</script>
->>>>>>> 1fdc2cd (feat:變更sass全域位置)
